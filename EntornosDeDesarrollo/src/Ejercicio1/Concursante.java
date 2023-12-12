718b948ca67157f0a82c8c68293e182d06f2608f
Dani <garzizamora@gmail.com>
2023-12-12 08:24:08 +0100

    bug fixes

--- EntornosDeDesarrollo/src/Ejercicio1/Concursante.java
+++ EntornosDeDesarrollo/src/Ejercicio1/Concursante.java
@@ -20,6 +20,54 @@ public class Concursante {
 		this.signoZodiaco = signoZodiaco;
 		this.cFavorito = cFavorito;
 	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the nFavorito
+	 */
+	public int getnFavorito() {
+		return nFavorito;
+	}
+	/**
+	 * @param nFavorito the nFavorito to set
+	 */
+	public void setnFavorito(int nFavorito) {
+		this.nFavorito = nFavorito;
+	}
+	/**
+	 * @return the signoZodiaco
+	 */
+	public String getSignoZodiaco() {
+		return signoZodiaco;
+	}
+	/**
+	 * @param signoZodiaco the signoZodiaco to set
+	 */
+	public void setSignoZodiaco(String signoZodiaco) {
+		this.signoZodiaco = signoZodiaco;
+	}
+	/**
+	 * @return the cFavorito
+	 */
+	public String getcFavorito() {
+		return cFavorito;
+	}
+	/**
+	 * @param cFavorito the cFavorito to set
+	 */
+	public void setcFavorito(String cFavorito) {
+		this.cFavorito = cFavorito;
+	}
 	
 	
 	
